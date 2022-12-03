004963954e1321160d754d8005528ad026df7cf7
qirall79 <belfatmi79@gmail.com>
2022-12-03 21:52:32 +0100

    Fix Shop Now button redirection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 
@@ -7,12 +8,12 @@ function App() {
     <div className="box-border w-100 h-screen bg-[url('./images/bg-apple.jpg')] bg-cover overflow-x-hidden ">
       <Navbar />
       <div className="w-100 h-[800px] px-8 md:px-20 xl:px-24 pt-52 ">
-        <a
+        <Link
           className="px-12 py-5 bg-white text-black font-semibold hover:text-lg hover:scale-125 transition-all duration-400"
-          href="/catalog"
+          to={"/catalog"}
         >
           Shop Now
-        </a>
+        </Link>
       </div>
     </div>
   );
